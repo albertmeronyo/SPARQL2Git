35f9862499467e061ff236c7425f48e8353760d1
Albert Meroño-Peñuela <albert.meronyo@gmail.com>
2017-02-28 21:12:10 +0100

    previous view with repos

--- src/github.py
+++ src/github.py
@@ -3,6 +3,9 @@
 import requests
 import logging
 from flask import jsonify
+from grlc.gquery import get_yaml_decorators
+import json
+from base64 import b64encode
 
 # SPARQL2Git modules
 import static
@@ -14,45 +17,111 @@ class SPARQL2GitHub():
 
         # self.user_sparql_repos(user_url)
 
-    def auth(self, user_info, access_token):
-        self.headers = { 'Accept' : 'application/json'}
-        self.params = {'access_token' : access_token, 'per_page' : '100'}
+    def auth(self, user_info, access_token, scope):
+        self.headers = { 'Accept' : 'application/json', 'Authorization': 'token {}'.format(access_token)}
 
         self.user_info = user_info
+        self.scope = scope
 
     def user_sparql_repos(self):
         # Gets SPARQL repos of username
 
-        repos_url = requests.get(self.user_info['url'], params=self.params, headers=self.headers).json()['repos_url']
+        repos_url = requests.get(self.user_info['url'], headers=self.headers).json()['repos_url']
         self.s2glogger.debug("Repos URL: {}".format(repos_url))
-        repos_array = requests.get(repos_url, params=self.params, headers=self.headers).json()
+        repos_array = requests.get(repos_url, params={'per_page' : "100"}, headers=self.headers).json()
 
-        for repo in repos_array:
-            repo_name = repo['name']
-            repo_html_url = repo['html_url']
-            contents_url = repo['contents_url'][:repo['contents_url'].rfind('/')]
-            file_array = requests.get(contents_url, params=self.params, headers=self.headers).json()
-            for file_object in file_array:
-                file_name = file_object['name']
-                if '.rq' in file_name or '.sparql' in file_name:
-                    if repo_name not in self.sparql_repos:
-                        self.sparql_repos[repo_name] = {'html_url' : repo_html_url, 'contents_url' : contents_url, 'files' : [ file_name ]}
-                    else:
-                        if file_name not in self.sparql_repos[repo_name]['files']:
-                            self.sparql_repos[repo_name]['files'].append(file_name)
+        # for repo in repos_array:
+        #     repo_name = repo['name']
+        #     repo_html_url = repo['html_url']
+        #     contents_url = repo['contents_url'][:repo['contents_url'].rfind('/')]
+        #     file_array = requests.get(contents_url, params={'per_page' : "100"}, headers=self.headers).json()
+        #     for file_object in file_array:
+        #         if 'name' not in file_object: # Repository is empty
+        #             break
+        #         file_name = file_object['name']
+        #         file_sha = file_object['sha']
+        #         if '.rq' in file_name or '.sparql' in file_name:
+        #             if repo_name not in self.sparql_repos:
+        #                 self.sparql_repos[repo_name] = {'html_url' : repo_html_url, 'contents_url' : contents_url, 'files' : [ {'file_name': file_name, 'sha' : file_sha }]}
+        #             else:
+        #                 if file_name not in [file_str['file_name'] for file_str in self.sparql_repos[repo_name]['files']]:
+        #                     self.sparql_repos[repo_name]['files'].append({'file_name': file_name, 'sha': file_sha})
 
-        self.s2glogger.debug("SPARQL repos: {}".format(self.sparql_repos))
+        # self.s2glogger.debug("SPARQL repos: {}".format(self.sparql_repos))
+        self.s2glogger.debug("SPARQL repos: {}".format(repos_array))
 
-        return self.sparql_repos
+        return repos_array
 
-    def files_in_repo(self, repo_name):
+    def files_in_repo(self, repo):
         # Gets SPARQL files in repo
 
-        return self.sparql_repos[repo_name]['files']
+        # return self.sparql_repos[repo_name]['files']
+        contents_url = 'https://api.github.com/repos/{}/{}/contents'.format(self.user_info['login'], repo)
+        file_array = requests.get(contents_url, params={'per_page' : "100"}, headers=self.headers).json()
+        files = []
+        for file_object in file_array:
+            if 'name' not in file_object: # Repository is empty
+                break
+            file_name = file_object['name']
+            # file_sha = file_object['sha']
+            if '.rq' in file_name or '.sparql' in file_name:
+                files.append(file_object)
+                # else:
+                #     if file_name not in [file_str['file_name'] for file_str in self.sparql_repos[repo_name]['files']]:
+                #         self.sparql_repos[repo_name]['files'].append({'file_name': file_name, 'sha': file_sha})
 
-    def query_raw_content(self, repo_name, file_name):
+        return files
 
-        query_text = requests.get('https://raw.githubusercontent.com/{}/{}/master/{}'.format(self.user_info['login'], repo_name, file_name), params=self.params, headers=self.headers).text
+    def query_contents(self, repo_name, file_name):
+        # JSON object with content of specified query, separating query from metadata
+
+        query_text = requests.get('https://raw.githubusercontent.com/{}/{}/master/{}'.format(self.user_info['login'], repo_name, file_name), headers=self.headers).text
         self.s2glogger.debug("Query text is: {}".format(query_text))
 
-        return query_text
+        return jsonify(get_yaml_decorators(query_text))
+
+    def create_repo(self, repo_name, description=None):
+        # Creates repo for the authenticated user
+
+        data = { "name": repo_name,
+                 "description": description,
+                 "homepage": None,
+                 "private": False,
+                 "has_issues": True,
+                 "has_wiki": True }
+        self.s2glogger.debug("Requesting new repo with data: {}".format(json.dumps(data)))
+        resp = requests.post('https://api.github.com/user/repos', headers=self.headers, data=json.dumps(data)).json()
+        self.s2glogger.debug("Request to create SPARQL repository returned status {}".format(resp))
+
+        return jsonify(resp)
+
+    def create_query(self, repo, name):
+        # Creates file 'name' for user and repo
+
+        data = { "message": "Created new SPARQL query",
+                 "committer": {
+                    "name": self.user_info['name'],
+                    "email": self.user_info['email']
+                 },
+                 "content": b64encode("SELECT * WHERE {?s ?p ?o}") }
+        self.s2glogger.debug("Requesting new query with data: {}".format(json.dumps(data)))
+        resp = requests.put('https://api.github.com/repos/{}/{}/contents/{}'.format(self.user_info['login'], repo, name), headers=self.headers, data=json.dumps(data)).json()
+        self.s2glogger.debug("Request to create SPARQL query file returned status {}".format(resp))
+
+        return jsonify(resp)
+
+    def commit_query(self, repo, name, commit, sha, content):
+        # Commits file
+
+        data = { "message": commit,
+                 "committer": {
+                    "name": self.user_info['login'],
+                    "email": self.user_info['email']
+                 },
+                 "content": b64encode(content),
+                 "sha": sha }
+        self.s2glogger.debug("Requesting new query with data: {}".format(json.dumps(data)))
+        resp = requests.put('https://api.github.com/repos/{}/{}/contents/{}'.format(self.user_info['login'], repo, name), headers=self.headers, data=json.dumps(data)).json()
+        self.s2glogger.debug("Request to create SPARQL query file returned status {}".format(resp))
+
+        return jsonify(resp)
--- src/server.py
+++ src/server.py
@@ -1,15 +1,17 @@
 #!/usr/bin/env python
 
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, make_response
 import logging
 import requests
 from ConfigParser import SafeConfigParser
+from json import dumps
 
 # SPARQL2Git modules
 import static
 from github import SPARQL2GitHub
 
 app = Flask(__name__)
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0 # Avoid browser caching of static assets
 
 # Set logging format
 logging.basicConfig(level=logging.DEBUG, format=static.LOG_FORMAT)
@@ -37,31 +39,92 @@ def callback_oauth():
                     'accept' : 'json' }
     headers = {'Accept' : 'application/json'}
     r = requests.post('https://github.com/login/oauth/access_token', data = github_auth, headers=headers).json()
-    s2glogger.debug("Access token from GitHub: {}".format(r))
+    s2glogger.debug("Access token and scopes from GitHub: {}".format(r))
 
     access_token = r['access_token']
+    scope = r['scope']
     user_info = requests.get('https://api.github.com/user', params={'access_token' : access_token}, headers=headers).json()
     s2glogger.debug("User info: {}".format(user_info))
-    user_emails = requests.get('https://api.github.com/user/emails', params={'access_token' : access_token}, headers=headers).json()
-    s2glogger.debug("User emails: {}".format(user_emails))
 
-    gapi.auth(user_info, access_token)
+    gapi.auth(user_info, access_token, scope)
 
     sparql_repos = gapi.user_sparql_repos()
 
-    return render_template('browser.html', user_info=user_info, user_emails=user_emails, sparql_repos=sparql_repos)
+    return render_template('repos.html', user_info=user_info, sparql_repos=sparql_repos)
+
+@app.route("/edit", methods=["GET"])
+def edit():
+    repo = request.args.get('repo', '')
+
+    s2glogger.debug("Preparing editor with user {}".format(gapi.user_info))
+    s2glogger.debug("Preparing editor with repo {}".format(repo))
+    files = gapi.files_in_repo(repo)
+
+    return render_template('browser.html', user_info=gapi.user_info, files=files, repo=repo)
 
 @app.route("/queries", methods=["GET"])
 def queries():
     # Queries for the user and repo in params
 
-    return jsonify(gapi.files_in_repo(request.args.get('repo', '')))
+    return make_response(dumps(gapi.files_in_repo(request.args.get('repo', ''))))
 
 @app.route("/query", methods=["GET"])
 def query():
-    # Raw content of specified query
+    # JSON object with content of specified query, separating query from metadata
 
-    return gapi.query_raw_content(request.args.get('repo', ''), request.args.get('file', ''))
+    return gapi.query_contents(request.args.get('repo', ''), request.args.get('file', ''))
+
+@app.route("/newrepo", methods=["POST"])
+def newrepo():
+
+    repo_name = request.form.get('repo_name', '')
+    repo_description = request.form.get('repo_description', '')
+    resp = gapi.create_repo(repo_name, repo_description)
+
+    return resp
+
+# @app.route("/newquery", methods=["POST"])
+# def newquery():
+#
+#     query_name = request.form.get('query_name', '')
+#     repo = request.form.get('repo', '')
+#     resp = gapi.create_query(repo, query_name)
+#
+#     return resp
+
+@app.route("/commitquery", methods=["POST"])
+def commitquery():
+
+    query_name = request.form.get('query_name', '')
+    commit = request.form.get('commit', '')
+    sha = request.form.get('sha', '')
+    content = request.form.get('content', '')
+    repo = request.form.get('repo', '')
+    content = request.form.get('content', '')
+    summary = request.form.get('summary', '')
+    endpoint = request.form.get('endpoint' , '')
+    tags = request.form.get('tags', '')
+    enum = request.form.get('enumerate', '')
+    method = request.form.get('method', '')
+    pagination = request.form.get('pagination', '')
+
+    full_content = "#+ summary: {}\n#+ endpoint: {}\n".format(summary, endpoint)
+    if tags:
+        full_content += "#+ tags:\n"
+        for tag in tags.split(','):
+            full_content += "#+   - {}\n".format(tag)
+    if enum:
+        full_content += "#+ enumerate:\n"
+        for en in enum:
+            full_content += "#+   - {}\n".format(en)
+    full_content += "#+ method: {}\n#+ pagination: {}\n\n".format(method, pagination)
+    full_content += content
+
+    s2glogger.debug("Committing full contents: {}".format(full_content))
+
+    resp = gapi.commit_query(repo, query_name, commit, sha, full_content)
+
+    return resp
 
 if __name__ == "__main__":
     app.run(debug=True)
